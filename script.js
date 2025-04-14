38a657fd7ccf70a8fecc0160f0a8433faa5e332d
Nikoszampe17 <zampetakisnikos@gmail.com>
2025-04-14 15:57:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+<script>
+    var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    });
+  </script>
