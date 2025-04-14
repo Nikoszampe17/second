9ca39cc069f744233ca1dbb81a47d8387183da25
Nikoszampe17 <zampetakisnikos@gmail.com>
2025-04-14 17:54:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Initialize Swiper
 var swiper = new Swiper(".mySwiper", {
   loop: true,
-  slidesPerView: 1,
+  
+  slidesPerView: 3,
+breakpoints: {
+  768: {
+    slidesPerView: 3,
+  },
+  480: {
+    slidesPerView: 2,
+  },
+  0: {
+    slidesPerView: 1,
+  }
+}
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-button-next",
