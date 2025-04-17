e681835634ed129becf6a5f809ea73d314abb108
Nikoszampe17 <zampetakisnikos@gmail.com>
2025-04-17 13:51:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+  const hamburger = document.getElementById('hamburger');
+  const navbar = document.getElementById('navbar');
+
+  hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+
